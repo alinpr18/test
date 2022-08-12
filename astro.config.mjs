061f38f61bc45b72493772d9f26947e5a1d90b27
Ali Paredes <alinorberto168@gmail.com>
2022-08-12 16:09:37 -0400

    🔥 Tests

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://alinpr18.github.io",
-  base: "/test",
-});
+export default defineConfig({});
